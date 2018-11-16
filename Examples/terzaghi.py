553f3df490cbdb4d1ab97bc42222f6b4760624f4
Herminio Tasinafo Honorio <herminio@sinmec.ufsc.br>
2018-11-16 11:57:55 -0200

    Big modifications. Installation features implemented.

---
+++ Examples/terzaghi.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 15 15:08:02 2018
+
+@author: herminio
+"""
+
+from AnalyticalSolutions.Terzaghi import TerzaghisProblemAnalytical
+from PhysicalPropertyTools.PropertyParser import Properties
+from CGNSTools.CGNSReader import ReadCGNSFile
+import pylab as pl
+
+
+
+
+time = 1.0
+
+height = 6.0
+tao_0 = 1.0e+5
+rock = Properties( "../PhysicalPropertyTools/Json_Files/solid.json" )
+fluid = Properties( "../PhysicalPropertyTools/Json_Files/fluid.json" )
+terza = TerzaghisProblemAnalytical( height, tao_0, rock, fluid )
+z_a = terza.getPositionValues()
+p_a = terza.getPressureValuesConstTime(time, 400)
+
+
+
+g = ReadCGNSFile( "../CGNSTools/Results/Results.cgns" )
+    
+def fun(x,y,z):
+    if x == 1 and y == 1:
+        return True
+    else:
+        return False
+
+g.loadPointsOfInterest(fun)    
+g.sortPointsOfInterest(3)
+p_n = g.getFieldValuesAtTime('Pressure', time)
+z_n = g.getCoordinateZ()
+
+
+
+
+pl.plot( p_a, z_a, 'b-' )
+pl.plot( p_n, z_n, '.-' )
+pl.grid(True)
+pl.show()
---
+++ README.txt
@@ -0,0 +1,5 @@
+This package is intended to give support for handling results generated from geomechanical problems.
+Analytical solutions for Terzaghi's and Mandel's problems are provided, as well as tools for reading numerical results from CGNS files.
+
+Installation:
+$ sudo python3 setup.py install
